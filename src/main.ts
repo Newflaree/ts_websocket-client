997acdd1f11bccffd0951f70574aa61c4e4c0003
Camilo López <camiloj.lopez79@gmail.com>
2025-04-07 11:41:59 -0400

    Client - Clientes conectados

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>WebSockets - Client</h1>
+
     <span id='server-status'>offline</span>
+
+    <ul id='clients-ul'></ul>
   </div>
 `
 
